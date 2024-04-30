454e9dabc76552850746acca5f22e404ebd7eeaa
Bas Nijholt <bas@nijho.lt>
2024-04-30 12:52:21 -0700

    Add a TaskGraph

--- pipefunc/_pipefunc.py
+++ pipefunc/_pipefunc.py
@@ -30,6 +30,7 @@ from typing import (
     Generic,
     Iterable,
     Literal,
+    NamedTuple,
     Tuple,
     TypeVar,
     Union,
@@ -530,7 +531,10 @@ class _LazyFunction:
         "_result",
         "_evaluated",
         "_delayed_callbacks",
+        "_id",
     ]
+
+    _counter = 0
 
     def __init__(
         self,
@@ -545,6 +549,28 @@ class _LazyFunction:
         self._result = None
         self._evaluated = False
         self._delayed_callbacks: list[_LazyFunction] = []
+
+        self._id = _LazyFunction._counter
+        _LazyFunction._counter += 1
+
+        if _TASK_GRAPH is not None:
+            _TASK_GRAPH.graph.add_node(self._id, lazy_func=self)
+            _TASK_GRAPH.mapping[self._id] = self
+
+            def add_edge(arg: Any) -> None:
+                if isinstance(arg, _LazyFunction):
+                    _TASK_GRAPH.graph.add_edge(arg._id, self._id)
+                elif isinstance(arg, Iterable):
+                    for item in arg:
+                        if isinstance(item, _LazyFunction):
+                            _TASK_GRAPH.graph.add_edge(item._id, self._id)
+
+            for arg in self.args:
+                add_edge(arg)
+
+            if kwargs is not None:
+                for arg in kwargs.values():
+                    add_edge(arg)
 
     def add_delayed_callback(self, cb: _LazyFunction) -> None:
         """Add a delayed callback to the lazy function."""
@@ -563,6 +589,25 @@ class _LazyFunction:
             cb._result = result
             evaluate_lazy(cb)
         return result
+
+
+class TaskGraph(NamedTuple):
+    """A named tuple representing a task graph."""
+
+    graph: nx.DiGraph
+    mapping: dict[int, _LazyFunction]
+
+
+@contextlib.contextmanager
+def construct_dag() -> Generator[TaskGraph, None, None]:
+    """Create a directed acyclic graph (DAG) for a pipeline."""
+    global _TASK_GRAPH
+    _TASK_GRAPH = TaskGraph(nx.DiGraph(), {})
+    yield _TASK_GRAPH
+    _TASK_GRAPH = None
+
+
+_TASK_GRAPH: nx.DiGraph | None = None
 
 
 class Pipeline:
