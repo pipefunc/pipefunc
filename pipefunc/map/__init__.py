fec127d386681d4b73bd98f412aeb1845baead01
Bas Nijholt <bas@nijho.lt>
2024-05-13 21:13:14 -0700

    Imports

--- pipefunc/map/__init__.py
+++ pipefunc/map/__init__.py
@@ -1,1 +1,7 @@
 """pipefunc.map: Modules that handle MapSpecs and its runs."""
+
+from pipefunc.map._filearray import FileArray
+from pipefunc.map._map import run_pipeline
+from pipefunc.map._mapspec import MapSpec
+
+__all__ = ["MapSpec", "run_pipeline", "FileArray"]
