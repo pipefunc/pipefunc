d1c32ead9edca279f3ea046960bd2b523200065b
Bas Nijholt <bas@nijho.lt>
2024-05-15 12:05:23 -0700

    Use updated logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 
-html_logo = "https://github.com/basnijholt/nijho.lt/raw/6b2c80b1b42507b46e71e66b06fd986dc7abef5c/content/project/pipefunc/featured.png"
+html_logo = "https://github.com/basnijholt/nijho.lt/raw/92b0aa820318f466388d828adf01120760255acf/content/project/pipefunc/featured.png"
 html_theme = "furo"
 html_static_path = ["_static"]
 
