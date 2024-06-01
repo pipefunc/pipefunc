ae4c90654772ec1e447aef8842bf9c14b4363821
Bas Nijholt <bas@nijho.lt>
2024-05-31 22:53:58 -0700

    Import

--- pipefunc/__init__.py
+++ pipefunc/__init__.py
@@ -1,6 +1,6 @@
 """PipeFunc: A Python library for defining, managing, and executing function pipelines."""
 
-from pipefunc import lazy, sweep
+from pipefunc import lazy, map, sweep
 from pipefunc._pipefunc import PipeFunc, pipefunc
 from pipefunc._pipeline import Pipeline
 from pipefunc._version import __version__
@@ -8,8 +8,9 @@ from pipefunc._version import __version__
 __all__ = [
     "__version__",
     "pipefunc",
-    "Pipeline",
     "PipeFunc",
-    "sweep",
+    "Pipeline",
     "lazy",
+    "map",
+    "sweep",
 ]
