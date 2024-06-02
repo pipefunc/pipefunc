61eb1d877e53381cfd9df9dd6c4f0bdb0f51ebb2
Bas Nijholt <basnijholt@gmail.com>
2024-06-01 23:49:49 -0700

    Update _pipeline.py

--- pipefunc/_pipeline.py
+++ pipefunc/_pipeline.py
@@ -791,14 +791,17 @@ class Pipeline:
         functions = self.sorted_functions if ordered else self.functions
         return [f.mapspec for f in functions if f.mapspec]
 
+    @functools.cached_property
     def mapspecs_as_strings(self) -> list[str]:
         """Return the MapSpecs for all functions in the pipeline as strings."""
         return [str(mapspec) for mapspec in self.mapspecs(ordered=True)]
 
+    @functools.cached_property
     def mapspec_dimensions(self: Pipeline) -> dict[str, int]:
         """Return the number of dimensions for each array parameter in the pipeline."""
         return mapspec_dimensions(self.mapspecs())
 
+    @functools.cached_property
     def mapspec_axes(self: Pipeline) -> dict[str, tuple[str, ...]]:
         """Return the axes for each array parameter in the pipeline."""
         return mapspec_axes(self.mapspecs())
