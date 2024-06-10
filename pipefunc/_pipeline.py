4da0514bf9138d8cc4a2ac009ed12a1b6344fc36
Bas Nijholt <bas@nijho.lt>
2024-06-10 15:18:12 -0700

    Add `Pipeline.update_renames`

--- pipefunc/_pipeline.py
+++ pipefunc/_pipeline.py
@@ -630,7 +630,7 @@ class Pipeline:
         """Update defaults to the provided keyword arguments.
 
         Automatically traverses the pipeline graph to find all functions that
-        depend on the updated defaults and updates their defaults as well.
+        that the defaults can be applied to.
 
         If `overwrite` is `False`, the new defaults will be added to the existing
         defaults. If `overwrite` is `True`, the existing defaults will be replaced
@@ -654,6 +654,32 @@ class Pipeline:
         if unused:
             unused_str = ", ".join(sorted(unused))
             msg = f"Unused keyword arguments: `{unused_str}`. These are not settable defaults."
+            raise ValueError(msg)
+
+    def update_renames(self, renames: dict[str, str], *, overwrite: bool = False) -> None:
+        """Update the renames for the pipeline.
+
+        Automatically traverses the pipeline graph to find all functions that
+        the renames can be applied to. Note that renames must be in terms of the
+        original parameter names, as defined in the function signature.
+
+        Parameters
+        ----------
+        renames
+            A dictionary mapping old parameter names to new parameter names.
+        overwrite
+            Whether to overwrite the existing renames. If ``False``, the new
+            renames will be added to the existing renames.
+
+        """
+        unused = set(renames.keys())
+        for f in self.functions:
+            update = {k: v for k, v in renames.items() if k in f.original_parameters}
+            unused -= set(update.keys())
+            f.update_renames(update, overwrite=overwrite)
+        if unused:
+            unused_str = ", ".join(sorted(unused))
+            msg = f"Unused keyword arguments: `{unused_str}`. These are not settable renames."
             raise ValueError(msg)
 
     @functools.cached_property
