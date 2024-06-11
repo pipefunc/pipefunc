pipefunc/pipefunc

commit d5d326de5277e267a478fe28cfa47e2efd192b3b

Reimplement `Pipeline.simplified_pipeline` using `NestedPipeFunc` (#156)

pipefunc/_simplify.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
 
-import inspect
-import warnings
 from collections import OrderedDict
-from typing import TYPE_CHECKING, Any, TypeAlias, Union
+from typing import TYPE_CHECKING, TypeAlias, Union
 
 import networkx as nx
 
-from pipefunc._pipefunc import PipeFunc
-from pipefunc._utils import at_least_tuple
+from pipefunc._pipefunc import NestedPipeFunc, PipeFunc
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Iterable
 
     import networkx as nx
 
@@ -21,29 +18,65 @@ if TYPE_CHECKING:
 _OUTPUT_TYPE: TypeAlias = Union[str, tuple[str, ...]]
 
 
-def _wrap_dict_to_tuple(
-    func: Callable[..., Any],
-    inputs: tuple[str, ...],
-    output_name: str | tuple[str, ...],
-) -> Callable[..., Any]:
-    sig = inspect.signature(func)
-    new_params = [
-        inspect.Parameter(name, inspect.Parameter.POSITIONAL_OR_KEYWORD) for name in inputs
-    ]
-    new_sig = sig.replace(parameters=new_params)
+def simplified_pipeline(
+    functions: list[PipeFunc],
+    graph: nx.DiGraph,
+    all_root_args: dict[_OUTPUT_TYPE, tuple[str, ...]],
+    node_mapping: dict[_OUTPUT_TYPE, PipeFunc | str],
+    output_name: _OUTPUT_TYPE,
+    *,
+    conservatively_combine: bool = False,
+) -> Pipeline:
+    """Simplify pipeline with combined function nodes.
 
-    def call(*args: Any, **kwargs: Any) -> Any:
-        """Call the pipeline function with the root arguments."""
-        bound = new_sig.bind(*args, **kwargs)
-        bound.apply_defaults()
-        r = func(**bound.arguments)
-        if isinstance(output_name, tuple):
-            return tuple(r[k] for k in output_name)
-        return r[output_name]
+    Generate a simplified version of the pipeline where combinable function
+    nodes have been merged into single function nodes.
 
-    call.__signature__ = new_sig  # type: ignore[attr-defined]
+    This method identifies combinable nodes in the pipeline's execution
+    graph (i.e., functions that share the same root arguments) and merges
+    them into single `NestedPipeFunc` nodes. This results in a simplified pipeline
+    where each key function only depends on nodes that cannot be further
+    combined.
 
-    return call
+    Returns
+    -------
+        The simplified version of the pipeline.
+
+    """
+    from pipefunc import PipeFunc, Pipeline
+
+    func = node_mapping[output_name]
+    assert isinstance(func, PipeFunc)
+    combinable_nodes = _identify_combinable_nodes(
+        func,
+        graph,
+        all_root_args,
+        conservatively_combine=conservatively_combine,
+    )
+    if not combinable_nodes:
+        msg = "No combinable nodes found, the pipeline cannot be simplified."
+        raise ValueError(msg)
+
+    # Simplify the combinable_nodes dictionary by replacing any nodes that
+    # can be combined with their own dependencies, so that each key in the
+    # dictionary only depends on nodes that cannot be further combined.
+    combinable_nodes = _combine_nodes(combinable_nodes)
+    # Sort to ensure deterministic output
+    sorted_nodes = {k: _sort(combinable_nodes[k]) for k in _sort(combinable_nodes.keys())}
+    to_combine_flat = _flatten_dict(sorted_nodes)
+    simple = Pipeline([f for f in functions if f not in to_combine_flat])
+    for base, combine in sorted_nodes.items():
+        nested = NestedPipeFunc([base, *combine], output_name=base.output_name)
+        simple.add(nested)
+    return simple
+
+
+def _sort(funcs: Iterable[PipeFunc]) -> list[PipeFunc]:
+    return sorted(funcs, key=lambda f: f.output_name)
+
+
+def _flatten_dict(d: dict[PipeFunc, list[PipeFunc]]) -> list[PipeFunc]:
+    return [v for k, lst in d.items() for v in [k, *lst]]
 
 
 def _identify_combinable_nodes(
@@ -182,60 +215,6 @@ def _combine_nodes(
     return dict(combinable_nodes)
 
 
-def _get_signature(
-    combinable_nodes: dict[PipeFunc, set[PipeFunc]],
-    graph: nx.DiGraph,
-) -> tuple[dict[PipeFunc, set[str]], dict[PipeFunc, set[str]]]:
-    """Retrieve the inputs and outputs for the signature of the combinable nodes.
-
-    This function generates a mapping of the inputs and outputs required for
-    each node in the combinable_nodes dictionary. For each node, it collects
-    the outputs of all nodes it depends on and the parameters it and its
-    dependent nodes require. In addition, it considers additional outputs
-    based on the dependencies in the graph. It then filters these lists to
-    ensure that no parameter is considered an output and no output is
-    considered a parameter.
-
-    Parameters
-    ----------
-    combinable_nodes
-        Dictionary containing the nodes that can be combined together.
-        The keys of the dictionary are the nodes that can be combined,
-        and the values are sets of nodes that they depend on.
-    graph
-        The directed graph of the pipeline functions. Each node represents a
-        function, and each edge represents a dependency relationship between
-        functions.
-
-    Returns
-    -------
-    all_inputs
-        Dictionary where keys are nodes and values are sets of parameter
-        names that the node and its dependent nodes require.
-    all_outputs
-        Dictionary where keys are nodes and values are sets of output names
-        that the node and its dependent nodes produce, plus additional output
-        names based on the dependency relationships in the graph.
-
-    """
-    all_inputs = {}
-    all_outputs = {}
-    for node, to_replace in combinable_nodes.items():
-        outputs = set(at_least_tuple(node.output_name))
-        parameters = set(node.parameters)
-        additional_outputs = set()  # parameters that are outputs to other functions
-        for f in to_replace:
-            outputs |= set(at_least_tuple(f.output_name))
-            parameters |= set(f.parameters)
-            for successor in graph.successors(f):
-                if successor not in to_replace and successor != node:
-                    edge = graph.edges[f, successor]
-                    additional_outputs |= set(at_least_tuple(edge["arg"]))
-        all_outputs[node] = (outputs - parameters) | additional_outputs
-        all_inputs[node] = parameters - outputs
-    return all_inputs, all_outputs
-
-
 def _func_node_colors(
     functions: list[PipeFunc],
     combinable_nodes: dict[PipeFunc, set[PipeFunc]],
@@ -255,104 +234,3 @@ def _func_node_colors(
             func_node_colors.append(f"C{color_index}")
             color_index += 1
     return func_node_colors
-
-
-def simplified_pipeline(
-    functions: list[PipeFunc],
-    graph: nx.DiGraph,
-    all_root_args: dict[_OUTPUT_TYPE, tuple[str, ...]],
-    node_mapping: dict[_OUTPUT_TYPE, PipeFunc | str],
-    func_dependencies: list[_OUTPUT_TYPE],
-    output_name: _OUTPUT_TYPE,
-    *,
-    conservatively_combine: bool = False,
-) -> Pipeline:
-    """Simplify pipeline with combined function nodes.
-
-    Generate a simplified version of the pipeline where combinable function
-    nodes have been merged into single function nodes.
-
-    This method identifies combinable nodes in the pipeline's execution
-    graph (i.e., functions that share the same root arguments) and merges
-    them into single function nodes. This results in a simplified pipeline
-    where each key function only depends on nodes that cannot be further
-    combined.
-
-    Returns
-    -------
-        The simplified version of the pipeline.
-
-    Notes
-    -----
-    The pipeline simplification process works in the following way:
-
-    1.  Identify combinable function nodes in the execution graph by
-        checking if they share the same root arguments.
-    2.  Simplify the dictionary of combinable nodes by replacing any nodes
-        that can be combined with their dependencies.
-    3.  Generate the set of nodes to be skipped (those that will be merged).
-    4.  Get the input and output signatures for the combined nodes.
-    5.  Create new pipeline functions for the combined nodes, and add them
-        to the list of new functions.
-    6.  Add the remaining (non-combinable) functions to the list of new
-        functions.
-    7.  Generate a new pipeline with the new functions.
-
-    This process can significantly simplify complex pipelines, making them
-    easier to understand and potentially improving performance by simplifying
-    function calls.
-
-    """
-    from pipefunc import PipeFunc, Pipeline
-
-    func = node_mapping[output_name]
-    assert isinstance(func, PipeFunc)
-    combinable_nodes = _identify_combinable_nodes(
-        func,
-        graph,
-        all_root_args,
-        conservatively_combine=conservatively_combine,
-    )
-    if not combinable_nodes:
-        warnings.warn(
-            "No combinable nodes found, the pipeline cannot be simplified.",
-            UserWarning,
-            stacklevel=2,
-        )
-    # Simplify the combinable_nodes dictionary by replacing any nodes that
-    # can be combined with their own dependencies, so that each key in the
-    # dictionary only depends on nodes that cannot be further combined.
-    combinable_nodes = _combine_nodes(combinable_nodes)
-    skip = set.union(*combinable_nodes.values()) if combinable_nodes else set()
-    in_sig, out_sig = _get_signature(combinable_nodes, graph)
-    predecessors = [node_mapping[o] for o in func_dependencies]
-    head = node_mapping[output_name]
-    new_functions = []
-    for f in functions:
-        if f != head and f not in predecessors:
-            continue
-        if f in combinable_nodes:
-            inputs = tuple(sorted(in_sig[f]))
-            outputs = tuple(sorted(out_sig[f]))
-            if len(outputs) == 1:
-                outputs = outputs[0]  # type: ignore[assignment]
-            funcs = [f, *combinable_nodes[f]]
-            mini_pipeline = Pipeline(funcs)  # type: ignore[arg-type]
-            pfunc = mini_pipeline.func(f.output_name).call_full_output
-            f_combined = _wrap_dict_to_tuple(pfunc, inputs, outputs)
-            f_combined.__name__ = f"combined_{f.__name__}"
-            f_pipefunc = PipeFunc(
-                f_combined,
-                outputs,
-                profile=f.profile,
-                cache=f.cache,
-                save_function=f.save_function,
-            )
-            # Disable saving for all functions that are being combined
-            for f_ in funcs:
-                f_.save_function = None
-            f_pipefunc.parameters = tuple(inputs)
-            new_functions.append(f_pipefunc)
-        elif f not in skip:
-            new_functions.append(f)
-    return Pipeline(new_functions)  # type: ignore[arg-type]

tests/test_pipefunc.py
@@ -1389,3 +1389,19 @@ def test_axis_in_root_args():
 
     pipeline = Pipeline([f, g, h])
     assert pipeline.independent_axes_in_mapspecs("e") == {"i"}
+
+
+def test_nesting_funcs_with_bound():
+    @pipefunc(output_name="c")
+    def f(a, b):
+        return a + b
+
+    @pipefunc(output_name="d", bound={"b": "b2"})
+    def g(c, b):
+        return c + b
+
+    pipeline = Pipeline([f, g])
+    assert pipeline("d", a="a", b="b") == "abb2"
+    pipeline.nest_funcs(["c", "d"], "d")
+    assert pipeline("d", a="a", b="b") == "abb2"
+    assert len(pipeline.functions) == 1

tests/test_simplify.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import pytest
 
 from pipefunc import Pipeline, pipefunc
-from pipefunc._simplify import _combine_nodes, _get_signature, _identify_combinable_nodes
+from pipefunc._simplify import _combine_nodes, _identify_combinable_nodes
 
 
 def test_identify_combinable_nodes():
@@ -26,16 +26,8 @@ def test_identify_combinable_nodes():
         pass
 
     pipeline = Pipeline([f_d, f_e, f_i, f_gg])
-    combinable_nodes = _identify_combinable_nodes(
-        pipeline.output_to_func["i"],
-        pipeline.graph,
-        pipeline.all_root_args,
-        conservatively_combine=True,
-    )
-    assert combinable_nodes == {f_gg: {f_e}}
-    sig_in, sig_out = _get_signature(combinable_nodes, pipeline.graph)
-    assert sig_in == {f_gg: {"a", "x"}}
-    assert sig_out == {f_gg: {"gg", "h", "g"}}
+
+    # `conservatively_combine=False`
     combinable_nodes = _identify_combinable_nodes(
         pipeline.output_to_func["i"],
         pipeline.graph,
@@ -43,9 +35,27 @@ def test_identify_combinable_nodes():
         conservatively_combine=False,
     )
     assert combinable_nodes == {f_gg: {f_e}, f_i: {f_d}}
-    sig_in, sig_out = _get_signature(combinable_nodes, pipeline.graph)
-    assert sig_in == {f_gg: {"a", "x"}, f_i: {"g", "b", "gg", "x"}}
-    assert sig_out == {f_gg: {"gg", "h", "g"}, f_i: {"d", "i"}}
+    simple = pipeline.simplified_pipeline(conservatively_combine=False)
+    assert len(simple.functions) == 2
+    assert repr(simple.functions[0]) == "NestedPipeFunc(pipefuncs=[PipeFunc(f_gg), PipeFunc(f_e)])"
+    assert repr(simple.functions[1]) == "NestedPipeFunc(pipefuncs=[PipeFunc(f_i), PipeFunc(f_d)])"
+    assert simple.functions[0].output_name == "gg"
+    assert simple.functions[1].output_name == "i"
+
+    # `conservatively_combine=True`
+    combinable_nodes = _identify_combinable_nodes(
+        pipeline.output_to_func["i"],
+        pipeline.graph,
+        pipeline.all_root_args,
+        conservatively_combine=True,
+    )
+    assert combinable_nodes == {f_gg: {f_e}}
+    simple = pipeline.simplified_pipeline(conservatively_combine=True)
+    assert len(simple.functions) == 3
+    assert repr(simple.functions[0]) == "PipeFunc(f_d)"
+    assert repr(simple.functions[1]) == "PipeFunc(f_i)"
+    assert repr(simple.functions[2]) == "NestedPipeFunc(pipefuncs=[PipeFunc(f_gg), PipeFunc(f_e)])"
+    assert simple.functions[2].output_name == "gg"
 
 
 def test_conservatively_combine():
@@ -76,16 +86,11 @@ def test_conservatively_combine():
     assert combinable_nodes_true == {}
 
     # Test simplified_pipeline with conservatively_combine=True
-    simplified_pipeline_true = pipeline.simplified_pipeline(
-        "z",
-        conservatively_combine=True,
-    )
-    simplified_functions_true = simplified_pipeline_true.functions
-    assert len(simplified_functions_true) == 3
-    function_names_true = [f.__name__ for f in simplified_functions_true]
-    assert "f1" in function_names_true
-    assert "f2" in function_names_true
-    assert "f3" in function_names_true
+    with pytest.raises(ValueError, match="No combinable nodes found"):
+        _simplified_pipeline_true = pipeline.simplified_pipeline(
+            "z",
+            conservatively_combine=True,
+        )
 
     # Test with conservatively_combine=False
     combinable_nodes_false = _identify_combinable_nodes(
@@ -105,19 +110,18 @@ def test_conservatively_combine():
     assert len(simplified_functions_false) == 2
     function_names_false = [f.__name__ for f in simplified_functions_false]
     assert "f1" in function_names_false
-    assert "combined_f3" in function_names_false
+    assert "NestedPipeFunc_z" in function_names_false
 
     # Check that the combined function has the expected input and output arguments
-    combined_f3 = next(f for f in simplified_functions_false if f.__name__ == "combined_f3")
+    combined_f3 = next(f for f in simplified_functions_false if f.__name__ == "NestedPipeFunc_z")
     assert combined_f3.parameters == ("b", "x")
     assert combined_f3.output_name == "z"
 
     # Check that the simplified pipeline produces the same output as the original pipeline
     input_data = {"a": 2, "b": 3}
     original_output = pipeline("z", **input_data)
-    simplified_output_true = simplified_pipeline_true("z", **input_data)
     simplified_output_false = simplified_pipeline_false("z", **input_data)
-    assert original_output == simplified_output_true == simplified_output_false
+    assert original_output == simplified_output_false
 
     assert pipeline._func_node_colors() == ["C1", "C0", "C0"]
 
@@ -156,10 +160,6 @@ def test_identify_combinable_nodes2():
     simplified_combinable_nodes = _combine_nodes(combinable_nodes)
     assert simplified_combinable_nodes == expected
 
-    sig_in, sig_out = _get_signature(simplified_combinable_nodes, pipeline.graph)
-    assert sig_in == {m["f6"]: {"a", "b", "c", "d"}}
-    assert sig_out == {m["f6"]: {"f6"}}
-
     # Test simplified_pipeline
     simplified_pipeline = pipeline.simplified_pipeline()
     assert (
@@ -175,11 +175,11 @@ def test_identify_combinable_nodes2():
     # Check that the simplified pipeline has the expected function names
     function_names = [f.__name__ for f in simplified_functions]
     assert "f2" in function_names
-    assert "combined_f6" in function_names
+    assert "NestedPipeFunc_f6" in function_names
     assert "f7" in function_names
 
     # Check that the combined function has the expected input and output arguments
-    combined_f6 = next(f for f in simplified_functions if f.__name__ == "combined_f6")
+    combined_f6 = next(f for f in simplified_functions if f.__name__ == "NestedPipeFunc_f6")
     assert combined_f6.parameters == ("a", "b", "c", "d")
     assert combined_f6.output_name == "f6"
 

pipefunc/_pipeline.py
@@ -938,7 +938,6 @@ class Pipeline:
             graph=self.graph,
             all_root_args=self.all_root_args,
             node_mapping=self.node_mapping,
-            func_dependencies=self.func_dependencies(output_name),
             output_name=output_name,
             conservatively_combine=conservatively_combine,
         )

commit 23f6c9f5c6a0f98eef15bb8f450a02cb31e599eb

Fix regression introduced in #156

pipefunc/_pipefunc.py
@@ -801,7 +801,7 @@ def _validate_output_name(output_name: _OUTPUT_TYPE | None, all_outputs: tuple[s
     if output_name is None:
         return
     if not all(x in all_outputs for x in at_least_tuple(output_name)):
-        msg = f"The provided `output_name` should be a subset of the combined output names: {all_outputs}."
+        msg = f"The provided `{output_name=}` should be a subset of the combined output names: {all_outputs}."
         raise ValueError(msg)
 
 

pipefunc/_simplify.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING, TypeAlias, Union
 import networkx as nx
 
 from pipefunc._pipefunc import NestedPipeFunc, PipeFunc
+from pipefunc._utils import at_least_tuple
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -64,11 +65,27 @@ def simplified_pipeline(
     # Sort to ensure deterministic output
     sorted_nodes = {k: _sort(combinable_nodes[k]) for k in _sort(combinable_nodes.keys())}
     to_combine_flat = _flatten_dict(sorted_nodes)
-    simple = Pipeline([f for f in functions if f not in to_combine_flat])
-    for base, combine in sorted_nodes.items():
-        nested = NestedPipeFunc([base, *combine], output_name=base.output_name)
-        simple.add(nested)
-    return simple
+    funcs = [f for f in functions if f not in to_combine_flat]
+    all_inputs = {p for f in funcs for p in f.parameters}
+    nested_funcs = [[base, *combine] for base, combine in sorted_nodes.items()]
+    for i, to_combine in enumerate(nested_funcs):
+        # Get all outputs for functions to combine
+        current_outputs = [n for f in to_combine for n in at_least_tuple(f.output_name)]
+        # Get inputs to all functions except the current one
+        other_inputs = {
+            p for j, fs in enumerate(nested_funcs) if j != i for f in fs for p in f.parameters
+        } | all_inputs
+        outputs_for_others = set(current_outputs) & other_inputs
+        base = to_combine[0]
+        output_name = _output_name({*at_least_tuple(base.output_name), *outputs_for_others})
+        nested = NestedPipeFunc(to_combine, output_name=output_name)
+        funcs.append(nested)
+    return Pipeline(funcs)  # type: ignore[arg-type]
+
+
+def _output_name(names: set[str]) -> _OUTPUT_TYPE:
+    output_names = tuple(sorted(names))
+    return output_names[0] if len(output_names) == 1 else output_names
 
 
 def _sort(funcs: Iterable[PipeFunc]) -> list[PipeFunc]:

tests/test_pipefunc.py
@@ -1052,7 +1052,7 @@ def test_nested_func() -> None:
     assert nf(a=1, b=2) == 3
 
     # Check all exceptions
-    with pytest.raises(ValueError, match="The provided `output_name` should"):
+    with pytest.raises(ValueError, match="The provided `output_name='not_exist'` should"):
         nf = NestedPipeFunc(
             [PipeFunc(f, "f"), PipeFunc(g, "g")],
             output_name="not_exist",

tests/test_simplify.py
@@ -39,7 +39,7 @@ def test_identify_combinable_nodes():
     assert len(simple.functions) == 2
     assert repr(simple.functions[0]) == "NestedPipeFunc(pipefuncs=[PipeFunc(f_gg), PipeFunc(f_e)])"
     assert repr(simple.functions[1]) == "NestedPipeFunc(pipefuncs=[PipeFunc(f_i), PipeFunc(f_d)])"
-    assert simple.functions[0].output_name == "gg"
+    assert simple.functions[0].output_name == ("g", "gg")
     assert simple.functions[1].output_name == "i"
 
     # `conservatively_combine=True`
@@ -55,7 +55,8 @@ def test_identify_combinable_nodes():
     assert repr(simple.functions[0]) == "PipeFunc(f_d)"
     assert repr(simple.functions[1]) == "PipeFunc(f_i)"
     assert repr(simple.functions[2]) == "NestedPipeFunc(pipefuncs=[PipeFunc(f_gg), PipeFunc(f_e)])"
-    assert simple.functions[2].output_name == "gg"
+    assert simple.functions[1].output_name == "i"
+    assert simple.functions[2].output_name == ("g", "gg")
 
 
 def test_conservatively_combine():