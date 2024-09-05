b49dd66d567271e8147f42082a00371f531388ff
Bas Nijholt <bas@nijho.lt>
2024-09-05 10:06:14 -0700

    emoji

--- pipefunc/_pipeline.py
+++ pipefunc/_pipeline.py
@@ -136,7 +136,7 @@ class Pipeline:
       which is a requirement for storing results in a cache.
     - This function works for many common types but is not guaranteed to work for all types.
     - If `~pipefunc.cache.to_hashable` cannot make a value hashable, it falls back to using the `str` representation of the value.
-    - Caution: Using `str` representations can lead to unexpected behavior if they are not unique for different function calls!
+    - Caution ⛔️: Using `str` representations can lead to unexpected behavior if they are not unique for different function calls!
 
     The key difference is that ``pipeline.run``'s output is uniquely determined by the root arguments,
     while ``pipeline.map`` is not because it may contain reduction operations as described by `~pipefunc.map.MapSpec`.
