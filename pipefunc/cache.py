c32df280f2d8a7c4f6aa42c6a48d534405af4056
Bas Nijholt <bas@nijho.lt>
2024-09-09 11:30:17 -0700

    Use pickle for cache key, inspired by `diskcache` package
    https://github.com/grantjenks/python-diskcache/blob/ebfa37cd99d7ef716ec452ad8af4b4276a8e2233/diskcache/core.py#L147C1-L164C1

--- pipefunc/cache.py
+++ pipefunc/cache.py
@@ -8,6 +8,7 @@ import collections
 import functools
 import hashlib
 import pickle
+import pickletools
 import sys
 import time
 from contextlib import nullcontext, suppress
@@ -423,7 +424,9 @@ class DiskCache(_CacheBase):
             )
 
     def _get_file_path(self, key: Hashable) -> Path:
-        key_hash = hashlib.md5(repr(key).encode()).hexdigest()  # noqa: S324
+        data = pickle.dumps(key, protocol=pickle.HIGHEST_PROTOCOL)
+        result = pickletools.optimize(data)
+        key_hash = hashlib.md5(result).hexdigest()  # noqa: S324
         return self.cache_dir / f"{key_hash}.pkl"
 
     def get(self, key: Hashable) -> Any:
