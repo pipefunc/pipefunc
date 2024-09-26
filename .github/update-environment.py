c4bfd4313cb3d3430f788babd1ff49b369017931
Bas Nijholt <bas@nijho.lt>
2024-09-26 15:15:49 -0700

    .

--- .github/update-environment.py
+++ .github/update-environment.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     with open("pyproject.toml") as f:  # noqa: PTH123
         data = tomllib.loads(f.read())
 
-    sections = ("adaptive", "plotting", "xarray", "zarr", "widgets")
+    sections = ("adaptive", "plotting", "xarray", "zarr", "pandas", "widgets")
     # Generate environment.yml
     generate_environment_yml(
         data,
