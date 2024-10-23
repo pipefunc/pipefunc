6fd17981204e24bed28f2e35b48649ea7fd3e513
Bas Nijholt <bas@nijho.lt>
2024-10-23 11:57:43 -0700

    skip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,17 @@
 import importlib.util
 
-import pytest
+has_adaptive = importlib.util.find_spec("adaptive") is not None
+has_zarr = importlib.util.find_spec("zarr") is not None
+has_xarray = importlib.util.find_spec("xarray") is not None
+has_ipywidgets = importlib.util.find_spec("ipywidgets") is not None
 
+collect_ignore_glob = []
 
-def pytest_collect_file(parent, path):
-    if path.basename == "test_adaptive.py":
-        has_adaptive = importlib.util.find_spec("adaptive") is not None
-        if not has_adaptive:
-            pytest.skip("adaptive not installed", allow_module_level=True)
+if not has_adaptive:
+    collect_ignore_glob.append("*_adaptive*")
+if not has_zarr:
+    collect_ignore_glob.append("*_zarr*")
+if not has_xarray:
+    collect_ignore_glob.append("*_xarray*")
+if not has_ipywidgets:
+    collect_ignore_glob.append("*_widgets*")
