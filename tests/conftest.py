9e12455f85126a28e4085e331ed8bb9dee98a443
Bas Nijholt <bas@nijho.lt>
2024-10-23 12:20:42 -0700

    warn

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import importlib.util
+import warnings
 
 has_adaptive = importlib.util.find_spec("adaptive") is not None
 has_zarr = importlib.util.find_spec("zarr") is not None
@@ -8,10 +9,14 @@ has_ipywidgets = importlib.util.find_spec("ipywidgets") is not None
 collect_ignore_glob = []
 
 if not has_adaptive:
+    warnings.warn("adaptive not installed, skipping adaptive tests", stacklevel=2)
     collect_ignore_glob.append("*_adaptive*")
 if not has_zarr:
+    warnings.warn("zarr not installed, skipping zarr tests", stacklevel=2)
     collect_ignore_glob.append("*_zarr*")
 if not has_xarray:
+    warnings.warn("xarray not installed, skipping xarray tests", stacklevel=2)
     collect_ignore_glob.append("*_xarray*")
 if not has_ipywidgets:
+    warnings.warn("ipywidgets not installed, skipping ipywidgets tests", stacklevel=2)
     collect_ignore_glob.append("*_widgets*")
