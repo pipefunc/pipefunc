a0279f7873e9db7fc50d951c148d45bc36690543
Bas Nijholt <bas@nijho.lt>
2024-10-23 12:44:24 -0700

    simplify

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,11 @@
 import importlib.util
 import warnings
 
-has_adaptive = importlib.util.find_spec("adaptive") is not None
-has_zarr = importlib.util.find_spec("zarr") is not None
-has_xarray = importlib.util.find_spec("xarray") is not None
-has_ipywidgets = importlib.util.find_spec("ipywidgets") is not None
-
 collect_ignore_glob = []
 
 
-def skip_if_missing(name: str, has: bool, match: str | None = None) -> None:  # noqa: FBT001
+def skip_if_missing(name: str, match: str | None = None) -> None:
+    has = importlib.util.find_spec(name) is not None
     if not has:
         warnings.warn(
             f"{name} not installed, skipping {name} tests",
@@ -19,7 +15,7 @@ def skip_if_missing(name: str, has: bool, match: str | None = None) -> None:  # 
         collect_ignore_glob.append(f"*_{name}*" if match is None else match)
 
 
-skip_if_missing("adaptive", has_adaptive)
-skip_if_missing("zarr", has_zarr)
-skip_if_missing("xarray", has_xarray)
-skip_if_missing("ipywidgets", has_ipywidgets, match="*_widgets*")
+skip_if_missing("adaptive")
+skip_if_missing("zarr")
+skip_if_missing("xarray")
+skip_if_missing("ipywidgets", match="*_widgets*")
