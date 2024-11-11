98b67e6ead1492f2cf4c687d6106d2f8c1c9cb1e
Bas Nijholt <bas@nijho.lt>
2024-11-11 15:05:51 -0800

    test

--- tests/map/test_run_dynamic_internal_shape.py
+++ tests/map/test_run_dynamic_internal_shape.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+
 from pipefunc import Pipeline, pipefunc
 from pipefunc.map._load import load_outputs
 from pipefunc.typing import Array  # noqa: TCH001
@@ -34,3 +36,24 @@ def test_dynamic_internal_shape(tmp_path: Path) -> None:
     assert results["sum"].output == 12
     assert results["sum"].output_name == "sum"
     assert load_outputs("sum", run_folder=tmp_path) == 12
+
+
+def test_exception():
+    @pipefunc(
+        output_name="x",
+        internal_shape=("'a' + 'b'",),  # doesn't evaluate to an int
+    )
+    def g(n: int) -> list[int]:
+        return list(range(n))
+
+    @pipefunc(output_name="y", mapspec="x[i] -> y[i]")
+    def h(x: int) -> int:
+        assert isinstance(x, int)
+        return 2 * x
+
+    pipeline = Pipeline([g, h])
+    with pytest.raises(
+        ValueError,
+        match="Expression `'a' + 'b'` must evaluate to an integer but it evaluated to `ab`.",
+    ):
+        pipeline.map({"n": 4}, run_folder=None, parallel=False)
