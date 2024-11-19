ed8d14ec6b0bfe8c72e0b8e59560fede6256d79a
Bas Nijholt <bas@nijho.lt>
2024-11-19 10:37:15 -0800

    Add setup.js

--- pipefunc/_widgets/graphvizsvg/src/graphvizsvg.js
+++ pipefunc/_widgets/graphvizsvg/src/graphvizsvg.js
@@ -4,6 +4,7 @@ if (typeof window !== "undefined") {
   window.jQuery = window.$ = jQuery;
 }
 import "bootstrap";
+import { setup } from "./setup";
 import { ColorUtil } from "./color";
 import { colorElement, restoreElement, highlight, tooltip, bringToFront, sendToBack } from "./styling";
 
@@ -80,7 +81,7 @@ class GraphvizSvg {
         (data) => {
           const svg = $("svg", data);
           this.$element.html(document.adoptNode(svg[0]));
-          this.setup();
+          setup(this);
         },
         "xml"
       );
@@ -88,7 +89,7 @@ class GraphvizSvg {
       if (this.options.svg) {
         this.$element.html(this.options.svg);
       }
-      this.setup();
+      setup(this);
     }
   }
 
@@ -110,134 +111,6 @@ class GraphvizSvg {
       options.shrink.y = this.convertToPx(options.shrink.y);
     }
     return options;
-  }
-
-  setup() {
-    const options = this.options;
-
-    // Save key elements in the graph for easy access
-    const $svg = $(this.$element.children("svg"));
-    const $graph = $svg.children("g:first");
-    this.$svg = $svg;
-    this.$graph = $graph;
-    this.$background = $graph.children("polygon:first"); // might not exist
-    this.$nodes = $graph.children(".node");
-    this.$edges = $graph.children(".edge");
-    this._nodesByName = {};
-    this._edgesByName = {};
-
-    // Add top-level class and copy background color to element
-    this.$element.addClass("graphviz-svg");
-    if (this.$background.length) {
-      this.$element.css("background", this.$background.attr("fill"));
-    }
-
-    // Setup all the nodes and edges
-    this.$nodes.each((_, el) => this.setupNodesEdges($(el), true));
-    this.$edges.each((_, el) => this.setupNodesEdges($(el), false));
-
-    // Remove the graph title element
-    const $title = this.$graph.children("title");
-    this.$graph.attr("data-name", $title.text());
-    $title.remove();
-
-    // Notify when setup is complete
-    if (options.ready) {
-      options.ready.call(this);
-    }
-  }
-
-  setupNodesEdges($el, isNode) {
-    const options = this.options;
-    const that = this; // Store reference to GraphvizSvg instance
-
-    // Save the colors of the paths, ellipses, and polygons
-    $el.find("polygon, ellipse, path").each((_, elem) => {
-      const $this = $(elem);
-      // Save original colors
-      $this.data("graphviz.svg.color", {
-        fill: $this.attr("fill"),
-        stroke: $this.attr("stroke"),
-      });
-
-      // Shrink it if it's a node
-      if (isNode && options.shrink) {
-        this.scaleNode($this);
-      }
-    });
-
-    // Save the node name and check for user comments
-    const $title = $el.children("title");
-    if ($title.length) {
-      // Remove any compass points
-      const title = $title.text().replace(/:[snew][ew]?/g, "");
-      $el.attr("data-name", title);
-      $title.remove();
-      if (isNode) {
-        this._nodesByName[title] = $el[0];
-      } else {
-        this._edgesByName[title] = $el[0];
-      }
-      // Check for user-added comments
-      let previousSibling = $el[0].previousSibling;
-      while (previousSibling && previousSibling.nodeType !== 8) {
-        previousSibling = previousSibling.previousSibling;
-      }
-      if (previousSibling && previousSibling.nodeType === 8) {
-        const htmlDecode = (input) => {
-          const e = document.createElement("div");
-          e.innerHTML = input;
-          return e.childNodes[0].nodeValue;
-        };
-        const value = htmlDecode(previousSibling.nodeValue.trim());
-        if (value !== title) {
-          // User-added comment
-          $el.attr("data-comment", value);
-        }
-      }
-    }
-
-    // Remove namespace from a[xlink:title]
-    $el
-      .find("a")
-      .filter(function () {
-        return $(this).attr("xlink:title");
-      })
-      .each(function () {
-        const $a = $(this);
-        $a.attr("title", $a.attr("xlink:title"));
-        $a.removeAttr("xlink:title");
-        if (options.tooltips) {
-          options.tooltips.init.call(this, that.$element);
-        }
-      });
-  }
-
-  scaleNode($node) {
-    const dx = this.options.shrink.x;
-    const dy = this.options.shrink.y;
-    const tagName = $node.prop("tagName");
-    if (tagName === "ellipse") {
-      $node.attr("rx", parseFloat($node.attr("rx")) - dx);
-      $node.attr("ry", parseFloat($node.attr("ry")) - dy);
-    } else if (tagName === "polygon") {
-      // Scale manually
-      const bbox = $node[0].getBBox();
-      const cx = bbox.x + bbox.width / 2;
-      const cy = bbox.y + bbox.height / 2;
-      const pts = $node.attr("points").trim().split(" ");
-      const points = pts
-        .map((pt) => {
-          const [xStr, yStr] = pt.split(",");
-          const ox = parseFloat(xStr);
-          const oy = parseFloat(yStr);
-          const newX = (((cx - ox) / (bbox.width / 2)) * dx + ox).toFixed(2);
-          const newY = (((cy - oy) / (bbox.height / 2)) * dy + oy).toFixed(2);
-          return `${newX},${newY}`;
-        })
-        .join(" ");
-      $node.attr("points", points);
-    }
   }
 
   convertToPx(val) {
