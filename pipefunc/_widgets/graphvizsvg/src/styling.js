e37fbc982633d2bd4413e90361cd21bce00efa83
Bas Nijholt <bas@nijho.lt>
2024-11-19 11:07:29 -0800

    fixes in styling.js

--- pipefunc/_widgets/graphvizsvg/src/styling.js
+++ pipefunc/_widgets/graphvizsvg/src/styling.js
@@ -1,51 +1,55 @@
+// styling.js
 export function highlight($nodesEdges, tooltips, context) {
   const options = context.options;
   const $everything = context.$nodes.add(context.$edges);
   if ($nodesEdges && $nodesEdges.length > 0) {
     // Dim all other elements
     $everything.not($nodesEdges).each((_, el) => {
-      context.colorElement($(el), options.highlight.unselected);
-      context.tooltip($(el));
+      colorElement($(el), options.highlight.unselected, context);
+      tooltip($(el), false, context);
     });
     $nodesEdges.each((_, el) => {
-      context.colorElement($(el), options.highlight.selected);
+      colorElement($(el), options.highlight.selected, context);
     });
     if (tooltips) {
-      context.tooltip($nodesEdges, true);
+      tooltip($nodesEdges, true, context);
     }
   } else {
     // Restore all elements
     $everything.each((_, el) => {
-      context.restoreElement($(el));
+      restoreElement($(el), context);
     });
-    context.tooltip($everything);
+    tooltip($everything, false, context);
   }
 }
+
+
 export function colorElement($el, getColor, context) {
   const bg = context.$element.css("background");
   $el.find("polygon, ellipse, path").each((_, elem) => {
-    const $context = $(elem);
-    const color = $context.data("graphviz.svg.color");
+    const $element = $(elem);
+    const color = $element.data("graphviz.svg.color");
 
     if (color.fill && color.fill !== "none") {
       const newFill = getColor(color.fill, bg);
-      $context.attr("fill", newFill);
+      $element.attr("fill", newFill);
     }
     if (color.stroke && color.stroke !== "none") {
       const newStroke = getColor(color.stroke, bg);
-      $context.attr("stroke", newStroke);
+      $element.attr("stroke", newStroke);
     }
   });
 }
+
 export function restoreElement($el, context) {
   $el.find("polygon, ellipse, path").each((_, elem) => {
-    const $context = $(elem);
-    const color = $context.data("graphviz.svg.color");
+    const $element = $(elem);
+    const color = $element.data("graphviz.svg.color");
     if (color.fill && color.fill != "none") {
-      $context.attr("fill", color.fill);
+      $element.attr("fill", color.fill);
     }
     if (color.stroke && color.stroke != "none") {
-      $context.attr("stroke", color.stroke);
+      $element.attr("stroke", color.stroke);
     }
   });
 }
@@ -53,20 +57,22 @@ export function restoreElement($el, context) {
 export function tooltip($elements, show, context) {
   const options = context.options;
   $elements.each(function () {
-    $(context)
+    $(this)
       .find("a[title]")
       .each(function () {
         if (show) {
-          options.tooltips.show.call(context);
+          options.tooltips.show.call(this);
         } else {
-          options.tooltips.hide.call(context);
+          options.tooltips.hide.call(this);
         }
       });
   });
 }
+
 export function bringToFront($elements, context) {
   $elements.detach().appendTo(context.$graph);
 }
+
 export function sendToBack($elements, context) {
   if (context.$background.length) {
     $elements.insertAfter(context.$background);
