pipefunc/pipefunc

commit 2281d68889a1debb954d0b70fbcfc74d86fd83ad

Allow to use cache for `Pipeline.map` (#291)

pipefunc/map/_run.py
@@ -13,6 +13,7 @@ import numpy as np
 import numpy.typing as npt
 
 from pipefunc._utils import at_least_tuple, dump, handle_error, load, prod
+from pipefunc.cache import to_hashable
 from pipefunc.map._mapspec import (
     MapSpec,
     _shape_to_key,
@@ -27,6 +28,7 @@ if TYPE_CHECKING:
     import xarray as xr
 
     from pipefunc import PipeFunc, Pipeline
+    from pipefunc.cache import _CacheBase
 
 
 _OUTPUT_TYPE: TypeAlias = str | tuple[str, ...]
@@ -118,6 +120,7 @@ def run(
                 outputs=outputs,
                 fixed_indices=fixed_indices,
                 executor=ex,
+                cache=pipeline.cache,
             )
 
     if persist_memory:  # Only relevant for memory based storage
@@ -264,6 +267,24 @@ def _pick_output(func: PipeFunc, output: Any) -> tuple[Any, ...]:
     )
 
 
+def _get_or_set_cache(
+    func: PipeFunc,
+    kwargs: dict[str, Any],
+    cache: _CacheBase | None,
+    compute_fn: Callable[[], Any],
+) -> Any:
+    if cache is None:
+        return compute_fn()
+    cache_key = (func.output_name, to_hashable(kwargs))
+
+    if cache_key in cache:
+        return cache.get(cache_key)
+
+    result = compute_fn()
+    cache.put(cache_key, result)
+    return result
+
+
 _EVALUATED_RESOURCES = "__pipefunc_internal_evaluated_resources__"
 
 
@@ -273,16 +294,21 @@ def _run_iteration(
     shape: tuple[int, ...],
     shape_mask: tuple[bool, ...],
     index: int,
+    cache: _CacheBase | None,
 ) -> Any:
     selected = _select_kwargs(func, kwargs, shape, shape_mask, index)
-    if callable(func.resources) and func.mapspec is not None and func.resources_scope == "map":  # type: ignore[has-type]
-        selected[_EVALUATED_RESOURCES] = func.resources(kwargs)  # type: ignore[has-type]
-    try:
-        return func(**selected)
-    except Exception as e:
-        handle_error(e, func, selected)
-        # handle_error raises but mypy doesn't know that
-        raise  # pragma: no cover
+
+    def compute_fn() -> Any:
+        if callable(func.resources) and func.mapspec is not None and func.resources_scope == "map":  # type: ignore[has-type]
+            selected[_EVALUATED_RESOURCES] = func.resources(kwargs)  # type: ignore[has-type]
+        try:
+            return func(**selected)
+        except Exception as e:
+            handle_error(e, func, selected)
+            # handle_error raises but mypy doesn't know that
+            raise  # pragma: no cover
+
+    return _get_or_set_cache(func, selected, cache, compute_fn)
 
 
 def _run_iteration_and_process(
@@ -292,8 +318,9 @@ def _run_iteration_and_process(
     shape: tuple[int, ...],
     shape_mask: tuple[bool, ...],
     file_arrays: Sequence[StorageBase],
+    cache: _CacheBase | None = None,
 ) -> tuple[Any, ...]:
-    output = _run_iteration(func, kwargs, shape, shape_mask, index)
+    output = _run_iteration(func, kwargs, shape, shape_mask, index, cache)
     outputs = _pick_output(func, output)
     _update_file_array(func, file_arrays, shape, shape_mask, index, outputs)
     return outputs
@@ -410,14 +437,14 @@ class _MapSpecArgs(NamedTuple):
 def _prepare_submit_map_spec(
     func: PipeFunc,
     kwargs: dict[str, Any],
-    shapes: dict[_OUTPUT_TYPE, tuple[int, ...]],
-    shape_masks: dict[_OUTPUT_TYPE, tuple[bool, ...]],
+    run_info: RunInfo,
     store: dict[str, StorageBase],
     fixed_indices: dict[str, int | slice] | None,
+    cache: _CacheBase | None = None,
 ) -> _MapSpecArgs:
     assert isinstance(func.mapspec, MapSpec)
-    shape = shapes[func.output_name]
-    mask = shape_masks[func.output_name]
+    shape = run_info.shapes[func.output_name]
+    mask = run_info.shape_masks[func.output_name]
     file_arrays = [store[name] for name in at_least_tuple(func.output_name)]
     result_arrays = _init_result_arrays(func.output_name, shape)
     process_index = functools.partial(
@@ -427,6 +454,7 @@ def _prepare_submit_map_spec(
         shape=shape,
         shape_mask=mask,
         file_arrays=file_arrays,
+        cache=cache,
     )
     fixed_mask = _mask_fixed_axes(fixed_indices, func.mapspec, shape, mask)
     existing, missing = _existing_and_missing_indices(file_arrays, fixed_mask)  # type: ignore[arg-type]
@@ -482,7 +510,12 @@ def _maybe_load_single_output(
     return None, False
 
 
-def _submit_single(func: PipeFunc, kwargs: dict[str, Any], run_folder: Path) -> Any:
+def _submit_single(
+    func: PipeFunc,
+    kwargs: dict[str, Any],
+    run_folder: Path,
+    cache: _CacheBase | None,
+) -> Any:
     # Load the output if it exists
     output, exists = _maybe_load_single_output(func, run_folder)
     if exists:
@@ -490,12 +523,16 @@ def _submit_single(func: PipeFunc, kwargs: dict[str, Any], run_folder: Path) -> 
 
     # Otherwise, run the function
     _load_file_array(kwargs)
-    try:
-        return func(**kwargs)
-    except Exception as e:
-        handle_error(e, func, kwargs)
-        # handle_error raises but mypy doesn't know that
-        raise  # pragma: no cover
+
+    def compute_fn() -> Any:
+        try:
+            return func(**kwargs)
+        except Exception as e:
+            handle_error(e, func, kwargs)
+            # handle_error raises but mypy doesn't know that
+            raise  # pragma: no cover
+
+    return _get_or_set_cache(func, kwargs, cache, compute_fn)
 
 
 def _maybe_load_file_array(x: Any) -> Any:
@@ -528,10 +565,11 @@ def _run_and_process_generation(
     outputs: dict[str, Result],
     fixed_indices: dict[str, int | slice] | None,
     executor: Executor | None,
+    cache: _CacheBase | None = None,
 ) -> None:
     tasks: dict[PipeFunc, _KwargsTask] = {}
     for func in generation:
-        tasks[func] = _submit_func(func, run_info, store, fixed_indices, executor)
+        tasks[func] = _submit_func(func, run_info, store, fixed_indices, executor, cache)
     for func in generation:
         _outputs = _process_task(func, tasks[func], run_info.run_folder, store, executor)
         outputs.update(_outputs)
@@ -543,21 +581,15 @@ def _submit_func(
     store: dict[str, StorageBase],
     fixed_indices: dict[str, int | slice] | None,
     executor: Executor | None,
+    cache: _CacheBase | None = None,
 ) -> _KwargsTask:
     kwargs = _func_kwargs(func, run_info, store)
     if func.mapspec and func.mapspec.inputs:
-        args = _prepare_submit_map_spec(
-            func,
-            kwargs,
-            run_info.shapes,
-            run_info.shape_masks,
-            store,
-            fixed_indices,
-        )
+        args = _prepare_submit_map_spec(func, kwargs, run_info, store, fixed_indices, cache)
         r = _maybe_parallel_map(args.process_index, args.missing, executor)
         task = r, args
     else:
-        task = _maybe_submit(_submit_single, executor, func, kwargs, run_info.run_folder)
+        task = _maybe_submit(_submit_single, executor, func, kwargs, run_info.run_folder, cache)
     return _KwargsTask(kwargs, task)
 
 

tests/test_pipeline_cache.py
@@ -169,12 +169,16 @@ def test_cache_non_root_args() -> None:
 
 
 def test_sharing_defaults() -> None:
+    calls = {"f": 0, "g": 0}
+
     @pipefunc(output_name="c", defaults={"b": 1}, cache=True)
     def f(a, b):
+        calls["f"] += 1
         return a + b
 
     @pipefunc(output_name="d", cache=True)
     def g(b, c):
+        calls["g"] += 1
         return b + c
 
     pipeline = Pipeline([f, g], cache_type="simple")
@@ -184,8 +188,19 @@ def test_sharing_defaults() -> None:
         ("c", (("a", 1), ("b", 1))): 2,
         ("d", (("a", 1), ("b", 1))): 3,
     }
-    assert pipeline.map(inputs={"a": 1})["d"].output == 3
-    assert pipeline.map(inputs={"a": 1, "b": 2})["d"].output == 5
+    # Call again, should use cache
+    assert pipeline("d", a=1) == 3
+    assert calls == {"f": 1, "g": 1}
+    # reset calls because `map`'s keys are different anyway
+    calls["f"] = 0
+    calls["g"] = 0
+    for _ in range(2):
+        assert pipeline.map(inputs={"a": 1}, parallel=False)["d"].output == 3
+        assert calls == {"f": 1, "g": 1}
+    for _ in range(2):
+        # Call with different arguments
+        assert pipeline.map(inputs={"a": 1, "b": 2}, parallel=False)["d"].output == 5
+        assert calls == {"f": 2, "g": 2}
 
 
 def test_autoset_cache() -> None:
@@ -196,3 +211,32 @@ def test_autoset_cache() -> None:
     pipeline = Pipeline([f])
     assert pipeline.cache is not None
     assert isinstance(pipeline.cache, LRUCache)
+
+
+def test_cache_with_map():
+    calls = {"f": 0, "g": 0}
+
+    @pipefunc(
+        output_name="c",
+        defaults={"b": 1},
+        cache=True,
+        mapspec="a[i] -> c[i]",
+    )
+    def f(a, b):
+        calls["f"] += 1
+        return a + b
+
+    @pipefunc(output_name="d", cache=True)
+    def g(b, c):
+        calls["g"] += 1
+        return b + sum(c)
+
+    pipeline = Pipeline([f, g], cache_type="simple")
+    a = [1, 2, 3]
+    for _ in range(3):
+        assert pipeline.map(inputs={"a": a}, parallel=False)["d"].output == 10
+        assert calls == {"f": len(a), "g": 1}
+    for _ in range(3):
+        # Call with different arguments
+        assert pipeline.map(inputs={"a": a, "b": 2}, parallel=False)["d"].output == 14
+        assert calls == {"f": 2 * len(a), "g": 2}

pipefunc/_pipeline.py
@@ -64,8 +64,6 @@ if TYPE_CHECKING:
 _OUTPUT_TYPE: TypeAlias = str | tuple[str, ...]
 _CACHE_KEY_TYPE: TypeAlias = tuple[_OUTPUT_TYPE, tuple[tuple[str, Any], ...]]
 
-_empty = inspect.Parameter.empty
-
 
 class Pipeline:
     """Pipeline class for managing and executing a sequence of functions.
@@ -84,7 +82,7 @@ class Pipeline:
         Flag indicating whether profiling information should be collected.
         If ``None``, the value of each PipeFunc's profile attribute is used.
     cache_type
-        The type of cache to use.
+        The type of cache to use. See the notes below for more *important* information.
     cache_kwargs
         Keyword arguments passed to the cache constructor.
     validate_type_annotations
@@ -111,6 +109,37 @@ class Pipeline:
         the resources are not set. Either a dict or a `pipefunc.resources.Resources`
         instance can be provided. If provided, the resources in the `PipeFunc`
         instances are updated with the default resources.
+
+    Notes
+    -----
+    Important note about caching: The caching behavior differs between ``pipeline.map`` and
+    ``pipeline.run`` / ``pipeline(...)``.
+
+    1. For ``pipeline.run`` and ``pipeline(...)`` ("calling the pipeline as a function"):
+
+    - The cache key is computed based solely on the root arguments provided to the pipeline.
+    - Only the root arguments need to be hashable.
+    - The root arguments uniquely determine the output across the entire pipeline, allowing
+      caching to be simple and effective when computing the final result.
+
+    2. For ``pipeline.map``:
+
+    - The cache key is computed based on the input values of each `PipeFunc`.
+    - So a `PipeFunc` with ``cache=True`` must have hashable input values.
+    - When using ``pipeline.map(..., parallel=True)``, the cache itself will be serialized,
+      so one must use a cache that supports shared memory, such as `~pipefunc.cache.LRUCache`
+      with ``shared=True`` or uses a disk cache like `~pipefunc.cache.DiskCache`.
+
+    For both methods:
+
+    - The `pipefunc.cache.to_hashable` function is used to attempt to ensure that input values are hashable,
+      which is a requirement for storing results in a cache.
+    - This function works for many common types but is not guaranteed to work for all types.
+    - If `~pipefunc.cache.to_hashable` cannot make a value hashable, it falls back to using the `str` representation of the value.
+    - Caution ⛔️: Using `str` representations can lead to unexpected behavior if they are not unique for different function calls!
+
+    The key difference is that ``pipeline.run``'s output is uniquely determined by the root arguments,
+    while ``pipeline.map`` is not because it may contain reduction operations as described by `~pipefunc.map.MapSpec`.
 
     """
 

pipefunc/map/adaptive.py
@@ -34,6 +34,7 @@ if TYPE_CHECKING:
     import numpy.typing as npt
 
     from pipefunc import PipeFunc, Pipeline
+    from pipefunc.cache import _CacheBase
     from pipefunc.map._storage_base import StorageBase
     from pipefunc.map.adaptive_scheduler import AdaptiveSchedulerDetails
     from pipefunc.resources import Resources
@@ -250,6 +251,7 @@ def create_learners(
                     run_info=run_info,
                     store=store,
                     fixed_indices=_fixed_indices,  # might be None
+                    cache=pipeline.cache,
                     return_output=return_output,
                 )
                 if func.resources_scope == "element":
@@ -273,6 +275,7 @@ def _learner(
     run_info: RunInfo,
     store: dict[str, StorageBase],
     fixed_indices: dict[str, int | slice] | None,
+    cache: _CacheBase | None,
     *,
     return_output: bool,
 ) -> SequenceLearner:
@@ -283,6 +286,7 @@ def _learner(
             run_info=run_info,
             store=store,
             return_output=return_output,
+            cache=cache,
         )
         shape = run_info.shapes[func.output_name]
         mask = run_info.shape_masks[func.output_name]
@@ -352,6 +356,7 @@ def _execute_iteration_in_map_spec(
     func: PipeFunc,
     run_info: RunInfo,
     store: dict[str, StorageBase],
+    cache: _CacheBase | None,
     *,
     return_output: bool = False,
 ) -> tuple[Any, ...] | None:
@@ -370,7 +375,7 @@ def _execute_iteration_in_map_spec(
     kwargs = _func_kwargs(func, run_info, store)
     shape = run_info.shapes[func.output_name]
     mask = run_info.shape_masks[func.output_name]
-    outputs = _run_iteration_and_process(index, func, kwargs, shape, mask, file_arrays)
+    outputs = _run_iteration_and_process(index, func, kwargs, shape, mask, file_arrays, cache)
     if not return_output:
         return None
     return outputs if isinstance(func.output_name, tuple) else outputs[0]

commit 6f37e8747501fdf4aad171706a3fd8d9c7de49d7

BUG: Only use the cache when `cache=True` instead of always in `pipeline.map`
Feature and bug introduced in https://github.com/pipefunc/pipefunc/pull/291

pipefunc/map/_run.py
@@ -884,6 +884,7 @@ def _submit_func(
 ) -> _KwargsTask:
     kwargs = _func_kwargs(func, run_info, store)
     status = progress.progress_dict[func.output_name] if progress is not None else None
+    cache = cache if func.cache else None
     if func.mapspec and func.mapspec.inputs:
         args = _prepare_submit_map_spec(func, kwargs, run_info, store, fixed_indices, cache)
         r = _maybe_parallel_map(func, args.process_index, args.missing, executor, status, progress)

tests/test_pipeline_cache.py
@@ -214,7 +214,7 @@ def test_autoset_cache() -> None:
 
 @pytest.mark.parametrize("cache_type", ["simple", "lru", "hybrid", "disk"])
 def test_cache_with_map(cache_type, tmp_path: Path) -> None:
-    calls = {"f": 0, "g": 0}
+    calls = {"f": 0, "g": 0, "h": 0}
 
     @pipefunc(
         output_name="c",
@@ -231,6 +231,11 @@ def test_cache_with_map(cache_type, tmp_path: Path) -> None:
         calls["g"] += 1
         return b + sum(c)
 
+    @pipefunc(output_name="e", cache=False)
+    def h(d):
+        calls["h"] += 1
+        return d
+
     cache_kwargs: dict[str, Any]
     if cache_type == "disk":
         cache_kwargs = {"cache_dir": tmp_path}
@@ -238,12 +243,12 @@ def test_cache_with_map(cache_type, tmp_path: Path) -> None:
         cache_kwargs = {"shared": False}
     else:
         cache_kwargs = {}
-    pipeline = Pipeline([f, g], cache_type=cache_type, cache_kwargs=cache_kwargs)
+    pipeline = Pipeline([f, g, h], cache_type=cache_type, cache_kwargs=cache_kwargs)
     a = [1, 2, 3]
-    for _ in range(3):
+    for i in range(3):
         assert pipeline.map(inputs={"a": a}, parallel=False)["d"].output == 10
-        assert calls == {"f": len(a), "g": 1}
-    for _ in range(3):
+        assert calls == {"f": len(a), "g": 1, "h": i + 1}
+    for i in range(3):
         # Call with different arguments
         assert pipeline.map(inputs={"a": a, "b": 2}, parallel=False)["d"].output == 14
-        assert calls == {"f": 2 * len(a), "g": 2}
+        assert calls == {"f": 2 * len(a), "g": 2, "h": i + 4}