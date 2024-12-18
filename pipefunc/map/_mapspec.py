589f1f9317f162ceedc5dfbf7abfcd04b7aac46d
Bas Nijholt <bas@nijho.lt>
2024-12-17 16:41:21 -0800

    BUG: Fix case where first dim is "?" (#466)
    More general fix for https://github.com/pipefunc/pipefunc/pull/465

--- pipefunc/map/_mapspec.py
+++ pipefunc/map/_mapspec.py
@@ -463,8 +463,11 @@ def _get_common_dim(
         axis = array.axes.index(index)
         return input_shapes[array.name][axis]
 
-    dim, *rest = (_get_dim(x, index) for x in arrays)
-    if any(dim != x for x in rest if x != "?"):
+    dims = [dim for x in arrays if (dim := _get_dim(x, index)) != "?"]
+    if not dims:
+        return "?"
+    dim, *rest = dims
+    if any(dim != x for x in rest):
         arrs = ", ".join(x.name for x in arrays)
         msg = f"Dimension mismatch for arrays `{arrs}` along `{index}` axis."
         raise ValueError(msg)
