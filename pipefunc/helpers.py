31c7de6f24a45a1c5254d71eb7e607fc0d585b5d
Bas Nijholt <bas@nijho.lt>
2024-12-20 12:11:12 -0800

    ENH: Add `helpers.get_attribute_factory` (#481)

--- pipefunc/helpers.py
+++ pipefunc/helpers.py
@@ -63,3 +63,59 @@ def collect_kwargs(
     _wrapped.__signature__ = new_sig  # type: ignore[attr-defined]
     _wrapped.__name__ = function_name
     return _wrapped
+
+
+def get_attribute_factory(
+    attribute_name: str,
+    parameter_name: str,
+    parameter_annotation: type = inspect.Parameter.empty,
+    return_annotation: type = inspect.Parameter.empty,
+    function_name: str = "get_attribute",
+) -> Callable[[Any], Any]:
+    """Returns a callable that retrieves an attribute from its input parameter.
+
+    Parameters
+    ----------
+    attribute_name
+        The name of the attribute to access.
+    parameter_name
+        The name of the input parameter.
+    parameter_annotation
+        Optional, type annotation for the input parameter.
+    return_annotation
+        Optional, type annotation for the return value.
+    function_name
+        The ``__name__`` that is assigned to the returned callable.
+
+    Returns
+    -------
+        Callable that returns an attribute of its input parameter.
+
+    Examples
+    --------
+    This creates ``def get_data(obj: MyClass) -> int``:
+
+    >>> class MyClass:
+    ...     def __init__(self, data: int) -> None:
+    ...         self.data = data
+    >>> f = get_attribute_factory("data", parameter_name="obj", parameter_annotation=MyClass, return_annotation=int, function_name="get_data")
+    >>> f(MyClass(data=123))
+    123
+
+    """
+    param = inspect.Parameter(
+        parameter_name,
+        inspect.Parameter.POSITIONAL_OR_KEYWORD,
+        annotation=parameter_annotation,
+    )
+    sig = inspect.Signature(parameters=[param], return_annotation=return_annotation)
+
+    def _wrapped(*args: Any, **kwargs: Any) -> Any:
+        bound = sig.bind(*args, **kwargs)
+        bound.apply_defaults()
+        obj = bound.arguments[parameter_name]
+        return getattr(obj, attribute_name)
+
+    _wrapped.__signature__ = sig  # type: ignore[attr-defined]
+    _wrapped.__name__ = function_name
+    return _wrapped
