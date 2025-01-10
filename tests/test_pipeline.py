0e10c2230af97d7e9a8d6386e6eeaf3e3bea3a16
Bas Nijholt <bas@nijho.lt>
2025-01-10 13:45:45 -0800

    fix

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -998,7 +998,7 @@ def test_nested_pipefunc_in_pipeline_renames() -> None:
     func = pipeline.output_to_func["test.x"]
     assert func.renames == {"n": "test.n", "x": "test.x", "y": "test.y"}
     assert isinstance(func, NestedPipeFunc)
-    assert func.pipeline.functions[0].renames == {"n": "test.n"}
+    assert func.pipeline.functions[0].renames == {}
     r = pipeline.run("test.y", kwargs={"test.n": 2})
     assert r == 8
     r = pipeline.map(inputs={"test.n": 2})
