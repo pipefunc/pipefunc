6664e279e876d0f5c2966481d1aa4483f1043dbf
Bas Nijholt <bas@nijho.lt>
2025-02-05 10:28:18 -0800

    BUG: Fix ND mapspec with multiple outputs and `internal_shape`

--- pipefunc/map/_run.py
+++ pipefunc/map/_run.py
@@ -606,9 +606,11 @@ def _validate_internal_shape(
     internal_shape: tuple[int, ...],
     func: PipeFunc,
 ) -> None:
-    if np.shape(output) != internal_shape:
+    shape = np.shape(output)
+    # shape = np.shape(output)[: len(internal_shape)]
+    if shape != internal_shape:
         msg = (
-            f"Output shape {np.shape(output)} of function '{func.__name__}'"
+            f"Output shape {shape} of function '{func.__name__}'"
             f" (output '{func.output_name}') does not match the expected"
             f" internal shape {internal_shape} used in the `mapspec`"
             f" '{func.mapspec}'. This error typically occurs when"
@@ -617,6 +619,9 @@ def _validate_internal_shape(
             " consistent for all inputs."
         )
         raise ValueError(msg)
+
+
+# def _output_shape(output)
 
 
 def _update_result_array(
