2c433a585cd2fbfc82ebfe9cf9129b84b72ff60a
Bas Nijholt <bas@nijho.lt>
2025-03-19 14:39:36 -0700

    MAINT: Move `NestedPipeFunc` to own file

--- pipefunc/__init__.py
+++ pipefunc/__init__.py
@@ -1,7 +1,8 @@
 """PipeFunc: A Python library for defining, managing, and executing function pipelines."""
 
 from pipefunc import cache, lazy, resources, sweep, testing
-from pipefunc._pipefunc import ErrorSnapshot, NestedPipeFunc, PipeFunc, pipefunc
+from pipefunc._nested_pipefunc import NestedPipeFunc
+from pipefunc._pipefunc import ErrorSnapshot, PipeFunc, pipefunc
 from pipefunc._pipeline._base import Pipeline
 from pipefunc._variant_pipeline import VariantPipeline
 from pipefunc._version import __version__
