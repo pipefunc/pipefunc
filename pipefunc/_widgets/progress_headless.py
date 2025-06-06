e3bc02292f68bfd583e0c67c10620f16db11a7ae
Bas Nijholt <bas@nijho.lt>
2025-06-06 16:51:28 -0700

    .

--- pipefunc/_widgets/progress_headless.py
+++ pipefunc/_widgets/progress_headless.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import time
 from typing import TYPE_CHECKING, Any
 
 from pipefunc._widgets.progress_base import ProgressTrackerBase
@@ -34,27 +33,10 @@ class HeadlessProgressTracker(ProgressTrackerBase):
             auto_update=auto_update,
             in_async=in_async,
         )
-
-        if self.task is not None:
-            self._set_auto_update(auto_update)
+        self._set_auto_update(value=False)
 
     def update_progress(self, _: Any = None, *, force: bool = False) -> None:
         """Update the progress values."""
-        t_start = time.monotonic()
-        return_early = self._should_throttle_update(force)
-        for name, status in self.progress_dict.items():
-            if status.progress == 0 or name in self._marked_completed:
-                continue
-            if return_early and status.progress < 1.0:
-                return
-            if status.progress >= 1.0:
-                self._marked_completed.add(name)
-
-        if self._all_completed():
-            self._mark_completed()
-
-        self.last_update_time = time.monotonic()
-        self._update_sync_interval(self.last_update_time - t_start)
 
     def _mark_completed(self) -> None:
         self._completed = True
@@ -63,7 +45,6 @@ class HeadlessProgressTracker(ProgressTrackerBase):
         """Cancel the ongoing calculation."""
         if self.task is not None:
             self.task.cancel()
-        self.update_progress(force=True)
 
     def _update_auto_update_interval_text(self, new_interval: float) -> None:
         """Update the auto-update interval text."""
