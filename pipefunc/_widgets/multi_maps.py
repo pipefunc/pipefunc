c72a7fc1e7513063339305e0413767064d16ea8b
Bas Nijholt <bas@nijho.lt>
2025-06-13 12:02:01 -0700

    ENH: Add widget for running multiple maps

---
+++ pipefunc/_widgets/multi_maps.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from contextlib import contextmanager
+from typing import TYPE_CHECKING
+
+import IPython
+import ipywidgets
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+
+class MultiMaps:
+    def __init__(self, num_maps: int) -> None:
+        self.outputs = [ipywidgets.Output() for _ in range(num_maps)]
+        self.visible_outputs = {hash(output): False for output in self.outputs}
+        self.tab = ipywidgets.Tab(children=[])
+
+    def display(self) -> None:
+        IPython.display.display(self.tab)
+
+    def show_output(self, index: int) -> None:
+        self.visible_outputs[hash(self.outputs[index])] = True
+        self._sync()
+
+    def hide_output(self, index: int) -> None:
+        self.visible_outputs[hash(self.outputs[index])] = False
+        self._sync()
+
+    def _sync(self) -> None:
+        children = [output for output in self.outputs if self.visible_outputs[hash(output)]]
+        self.tab.children = children
+        for i_tab, output in enumerate(self.tab.children):
+            index = self.outputs.index(output)
+            self.tab.set_title(i_tab, str(index))
+
+    @contextmanager
+    def output_context(self, index: int) -> Generator[None, None, None]:
+        if not 0 <= index < len(self.outputs):
+            msg = f"Index {index} out of range for {len(self.outputs)} outputs"
+            raise IndexError(msg)
+        with self.outputs[index]:
+            yield
