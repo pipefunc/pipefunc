b377bf51c4e1996ed9f4dbffeefdf9cc818ef602
Bas Nijholt <bas@nijho.lt>
2025-06-13 13:24:26 -0700

    add basic test

---
+++ tests/test_output_tabs_widget.py
@@ -0,0 +1,17 @@
+from pipefunc._widgets.output_tabs import OutputTabs
+
+
+def test_output_tabs_widget() -> None:
+    tabs = OutputTabs(3)
+    tabs.display()
+    tabs.show_output(0)
+    tabs.show_output(1)
+    tabs.show_output(2)
+    tabs.hide_output(1)
+    tabs.hide_output(2)
+    with tabs.output_context(0):
+        print("Hello")
+    with tabs.output_context(1):
+        print("World")
+    with tabs.output_context(2):
+        print("!")
