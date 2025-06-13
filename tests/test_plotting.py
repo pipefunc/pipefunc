d549d7ed3a341282c175b6fc99ca7317e0adb00b
Bas Nijholt <bas@nijho.lt>
2025-06-13 14:40:23 -0700

    fix hv

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -23,13 +23,12 @@ has_graphviz_exec = shutil.which("dot") is not None
 
 
 @pytest.fixture(autouse=True)
-def patch_hv_extension():
-    """Patch hv.extension to avoid display issues in tests."""
+def patch_pyviz_comms():
     # To avoid https://github.com/holoviz/pyviz_comms/issues/137
     # Remove once https://github.com/holoviz/pyviz_comms/pull/138 is merged
     try:
-        with patch("holoviews.extension") as mock_extension:
-            yield mock_extension
+        with patch("pyviz_comms._in_ipython", new=False) as mock_in_ipython:
+            yield mock_in_ipython
     except ImportError:
         yield
 
