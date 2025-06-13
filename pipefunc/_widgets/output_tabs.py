142d32db55347758bb2257546c9beb58508ea956
Bas Nijholt <bas@nijho.lt>
2025-06-13 14:24:31 -0700

    .

--- pipefunc/_widgets/output_tabs.py
+++ pipefunc/_widgets/output_tabs.py
@@ -8,23 +8,28 @@ if TYPE_CHECKING:
 
 
 class OutputTabs:
-    def __init__(self, num_maps: int) -> None:
+    """A ``ipywidgets.Tab`` widget that contains ``ipywidgets.Output`` widgets."""
+
+    def __init__(self, num_outputs: int) -> None:
         from ipywidgets import Output, Tab
 
-        self.outputs: list[Output] = [Output() for _ in range(num_maps)]
+        self.outputs: list[Output] = [Output() for _ in range(num_outputs)]
         self._visible_outputs: dict[Output, bool] = dict.fromkeys(self.outputs, False)
         self.tab: Tab = Tab(children=[])
 
     def display(self) -> None:
+        """Display the ``ipywidgets.Tab`` widget."""
         import IPython
 
         IPython.display.display(self.tab)
 
     def show_output(self, index: int) -> None:
+        """Show the output at the given index."""
         self._visible_outputs[self.outputs[index]] = True
         self._sync()
 
     def hide_output(self, index: int) -> None:
+        """Hide the output at the given index."""
         self._visible_outputs[self.outputs[index]] = False
         self._sync()
 
@@ -37,6 +42,7 @@ class OutputTabs:
 
     @contextmanager
     def output_context(self, index: int) -> Generator[None, None, None]:
+        """Context manager to show the output at the given index."""
         if not 0 <= index < len(self.outputs):
             msg = f"Index {index} out of range for {len(self.outputs)} outputs"
             raise IndexError(msg)
