b32a21cb4f15f90e8dbd64f04c582e9cc8ac6abb
Bas Nijholt <bas@nijho.lt>
2025-11-04 09:06:01 -0800

    ENH: Support and require Zarr v3 (#523)

--- pipefunc/map/__init__.py
+++ pipefunc/map/__init__.py
@@ -27,11 +27,8 @@ __all__ = [
     "storage_registry",
 ]
 
-with _suppress(ImportError, AttributeError):
+with _suppress(ImportError):
     # ImportError: zarr not installed
-    # AttributeError: zarr v3 is not compatible with pipefunc
-    #   Blocked by: https://github.com/zarr-developers/zarr-python/issues/2617
-    #   TODO: Remove this block once zarr v3 is compatible with pipefunc
     from ._storage_array._zarr import ZarrFileArray, ZarrMemoryArray, ZarrSharedMemoryArray
 
     __all__ += ["ZarrFileArray", "ZarrMemoryArray", "ZarrSharedMemoryArray"]
